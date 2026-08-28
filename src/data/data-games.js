22e11048c551312ec0726b78f21f11b045690991
AlanGamerTorre <alangamertorre13@gmail.com>
2026-08-28 19:33:43 +0200

    arreglo geometrry dash 1.2

--- src/data/data-games.js
+++ src/data/data-games.js
@@ -27,7 +27,7 @@ export const games_database = [
     name: "Geometry Dash",
     category: "Plataformas",
     img: "geometry.webp",
-    id: "geometry",
+    id: "geometry/public",
   },
   {
     name: "Friday Night Funkin'",
