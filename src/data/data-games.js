fe247776b85f960ebe3c1f274c3b6a47bd6bede0
Alan <AlanGamerTorre13@gmail.com>
2026-08-28 19:39:05 +0200

    web 6.2

--- src/data/data-games.js
+++ src/data/data-games.js
@@ -27,7 +27,7 @@ export const games_database = [
     name: "Geometry Dash",
     category: "Plataformas",
     img: "geometry.webp",
-    id: "geometry/public",
+    id: "geometry/public/",
   },
   {
     name: "Friday Night Funkin'",
